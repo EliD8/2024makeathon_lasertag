34daac0f2a0de196b56499d8b96280ae5dee41c1
Eli Dumper <edumper@uwaterloo.ca>
2024-02-17 14:47:05 -0500

    hit sound test

--- laser_blaster/laser_blaster.ino
+++ laser_blaster/laser_blaster.ino
@@ -1,19 +1,33 @@
+#define SPEAKER_PIN 8  // Pin connected to the speaker
+
 const int analogPin = A0; // Analog pin to read from
-const int thresholdValue = 3; // Specified threshold value
+const int thresholdValue = 2; // Specified threshold value
+unsigned long previousMillis = 0; // will store last time LED was updated
+const int hitCooldown = 1000;
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication at 9600 baud rate
+  pinMode(SPEAKER_PIN, OUTPUT);
 }
 
 void loop() {
   int sensorValue = analogRead(analogPin); // Read voltage from analog pin
-  float voltage = sensorValue * (3.3 / 1023.0); 
+  float voltage = sensorValue * (3.3 / 1023.0); //True voltage
+  unsigned long currentMillis = millis();
+
   
   // Check if sensor value exceeds the threshold
-  if (voltage >= thresholdValue) {
+  if (voltage >= thresholdValue && currentMillis - previousMillis >= hitCooldown) {
+    previousMillis = currentMillis;
+    tone(SPEAKER_PIN, 1000);
     Serial.println("Hit!"); // Print a message indicating threshold reached
-    // Additional actions can be taken here
-    delay(1000); // Delay for readability, adjust as needed
+    delay(500);
+    tone(SPEAKER_PIN, 1000);
+    delay(200);
+    tone(SPEAKER_PIN, 500);
+
+
+
   }
   
  
